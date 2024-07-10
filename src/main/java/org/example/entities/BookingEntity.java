b35d13ee3b2dc132b19af581ad810a9df2170981
MacosTacos <ivanov.maxim0@yandex.ru>
2024-07-10 16:08:50 +0300

    Initial commit

---
+++ src/main/java/org/example/entities/BookingEntity.java
@@ -0,0 +1,11 @@
+package org.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reservations")
+public class BookingEntity {
+
+}
+
